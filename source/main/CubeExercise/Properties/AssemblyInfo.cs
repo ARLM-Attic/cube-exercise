0c8a4f300ca08236e0bad24f1d7d151645d768f3
SND\fancyf_cp <SND\fancyf_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2009-10-07 15:58:22 +0000

    Change the directory structure for adding some tools (not completed).
    git-svn-id: https://cubeexercise.svn.codeplex.com/svn@32288 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

--- source/main/CubeExercise/Properties/AssemblyInfo.cs
+++ source/main/CubeExercise/Properties/AssemblyInfo.cs
@@ -10,10 +10,6 @@ using System.Windows;
 [assembly: AssemblyTitle("CubeExercise")]
 [assembly: AssemblyDescription("http://cubeexercise.fanrui.net\nE-mail: cubeexercise@fanrui.net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Rui Fan")]
-[assembly: AssemblyProduct("CubeExercise")]
-[assembly: AssemblyCopyright("Copyright © Rui Fan 2009")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -41,15 +37,3 @@ using System.Windows;
 )]
 
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.305.0")]
